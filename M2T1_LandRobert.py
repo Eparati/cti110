dfcffafdebaa45978f2c025b1808fc72c7fc283f
Eparati <31897677+Eparati@users.noreply.github.com>
2017-09-12 13:10:46 -0400

    Add files via upload
    First Submission

---
+++ M2T1_LandRobert.py
@@ -0,0 +1,13 @@
+# CTI-110 
+# M2T1 - Sales Prediction
+# Robert Land
+# Thursday, September 07, 2017
+# A short program used to calculate total profit earned from a sale.
+
+#To input the total sales
+total_sales= float(input('Enter the projected sales: '))
+
+#To calculate the proft as 23% of the total sales
+profit= total_sales * 0.23
+
+print( 'The profit is $', format( profit, ',.2f'))
